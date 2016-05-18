5c0873086a23a3c50e36c4bc418ffd02b09e06a9
tzz6 <291232797@qq.com>
2016-05-18 11:18:30 +0800

    Index

--- src/main/java/com/tzz/web/controller/IndexController.java
+++ src/main/java/com/tzz/web/controller/IndexController.java
@@ -15,10 +15,6 @@ import com.tzz.ws.cxf.service.Orders;
 import com.tzz.ws.cxf.service.WsOrdersService;
 import com.tzz.ws.cxf.service.impl.WsOrdersServiceImpl;
 
-/***
- * 客户端
- *
- */
 @Controller
 public class IndexController {
 	
